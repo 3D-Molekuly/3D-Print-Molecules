150dce8b3d29abbf3c69acf9723b1e12cf902a94
Jakub Vávra <56688914+KubiV@users.noreply.github.com>
2024-10-20 11:17:55 +0200

    error styling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let error;
+    import { _ } from 'svelte-i18n';
+</script>
+
+<div class="error-container">
+    <h1>{$_('error')} {error?.status}</h1>
+    <p>{error?.message || $_('error_occurred')}</p>
+</div>
+
+<style>
+    .error-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 50vh;
+      text-align: center;
+    }
+
+    h1 {
+      font-size: 3rem;
+      color: red;
+    }
+
+    p {
+      font-size: 1.5rem;
+    }
+</style>

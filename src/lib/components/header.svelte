68502cec3bd02ec559fb1d9658d4001a5c2cfcd8
KubiV <kubiwan007@gmail.com>
2024-12-20 18:04:29 +0100

    button fix

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,7 +3,7 @@
   import { _, locale } from 'svelte-i18n';
   import { writable } from 'svelte/store';
   import { goto } from '$app/navigation'; // Import goto for navigation
-  import DarkmodeButton from './darkmodeButton.svelte';
+  import DarkmodeButton from '$lib/buttons/darkmodeButton.svelte';
 
   // Create a writable store to track the current locale
   let storedLocale = 'en';

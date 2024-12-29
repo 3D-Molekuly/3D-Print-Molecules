1e553fecdec91f067d0907fab4fcba12094e5356
KubiV <kubiwan007@gmail.com>
2024-12-29 18:22:43 +0100

    sw

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,15 +53,16 @@
 	  // Inside +layout.svelte or wherever you're registering the service worker
 	  if ('serviceWorker' in navigator) {
 		window.addEventListener('load', () => {
-			navigator.serviceWorker
-			.register('/service-worker.js')
-			.then(registration => {
+			addEventListener('load', function () {
+			navigator.serviceWorker.register('/service-worker.js')
+			.then((registration: ServiceWorkerRegistration) => {
 				console.log('Service Worker registered with scope:', registration.scope);
 			})
 			.catch(error => {
 				console.error('Service Worker registration failed:', error);
 			});
-		});
+			});
+		  });
 		}
 	});
 

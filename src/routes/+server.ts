4d3f97baeb621b1d25aff7f75bc6cd7799f4ceaf
KubiV <kubiwan007@gmail.com>
2025-01-27 20:57:04 +0100

    XML declaration error

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -7,8 +7,7 @@ export const GET: RequestHandler = async () => {
     { url: 'https://3d-print-molecules-web.vercel.app/cs/about', priority: 0.8 },
   ];
 
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
         .map(
